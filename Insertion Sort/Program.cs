41b8e195e7002a01ef8a6e57c2315e96338e9239
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-08-15 23:09:13 +0300

    Örnek kod inceleme

---
+++ Insertion Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InsertionSortExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 22,27,16,2,18,6};
+            int n = sayilar.Length;
+
+            InsertionSort(sayilar, n);
+            PrintArray(sayilar);
+        }
+
+        static void PrintArray(int[] dizi)
+        {
+            foreach (int num in dizi)
+                Console.Write(num + " ");
+            Console.WriteLine();
+        }
+
+        static void InsertionSort(int[] arr, int n)
+        {
+            int deger, j;
+            for (int i = 1; i < n; i++)
+            {
+                deger = arr[i];
+                j = i - 1;
+
+                while (j >= 0 && arr[j] > deger)
+                {
+                    arr[j + 1] = arr[j];
+                    j -= 1;
+                }
+                arr[j + 1] = deger;
+            }
+        }
+    }
+}
