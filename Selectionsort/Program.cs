0147a4e206a4f5ab27098eacc4a5b22c788840bb
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-08-17 12:27:07 +0300

    Kod incelemesi-üzerinde test denemeleri

---
+++ Selectionsort/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    public static int[] SelectionSort(int[] A, int n)
+    {
+        int tmp;
+        int min;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            min = i;
+
+            for (int j = i; j < n; j++)
+            {
+                if (A[j] < A[min])
+                {
+                    min = j;
+                }
+            }
+
+            tmp = A[i];
+            A[i] = A[min];
+            A[min] = tmp;
+
+            // Adım adım çıktıları ekrana yazdırma
+            Console.Write("Adım " + (i + 1) + ": ");
+            foreach (int num in A)
+                Console.Write(num + " ");
+            Console.WriteLine();
+        }
+
+        return A;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 7,3,5,8,2,9,4,15,6 };
+        int n = arr.Length;
+
+        Console.WriteLine("Başlangıç Dizisi:");
+        foreach (int num in arr)
+            Console.Write(num + " ");
+        Console.WriteLine();
+
+        SelectionSort(arr, n);
+
+        Console.WriteLine("Sıralanmış Dizi:");
+        foreach (int num in arr)
+            Console.Write(num + " ");
+        Console.WriteLine();
+    }
+}
